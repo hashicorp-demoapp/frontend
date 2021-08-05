094df5323a606e08e1aad0ee5f71a28b36311c86
Rajas Nathak <61021680+rajasnathak@users.noreply.github.com>
2021-08-05 16:10:21 -0500

    Update index.js

--- src/components/Boxes/index.js
+++ src/components/Boxes/index.js
@@ -8,6 +8,7 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import HomeIcon from "@material-ui/icons/Home";
 import TwitterIcon from '@material-ui/icons/Twitter';
+import styled from 'styled-components'
 
 import { socialMedia } from './social';
 
@@ -29,7 +30,27 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
+const Logo = styled.div` //division in HTML document and it styled with css below
+    position: absolute;
+    top: calc(95% - 109px);
+    
+    z-index: 100; 
+    svg {
+        fill: #ffffff; /* black */
+    }
+`
+const Title = styled.div`
+    position: relative; //positions it relative to the logo
+    left: 80px; //80px away from logo
+    font-family: montserrat;
+    font-size: 2em;
+    width: 100px;
+    font-weight: 70;
+    z-index: 100;
+    opacity: 0%;
+    color: #fff;
+    
+`
 
 
 const MuiBox = ({ color }) => {
@@ -53,7 +74,24 @@ const MuiBox = ({ color }) => {
           fontSize={24}
         >
                   
-
+        <Grid
+        item
+        component={"a"}
+        target="_blank"
+        rel="noreferrer noopener"
+        href={homepage}
+      >
+        <Logo>
+                <a href="/">
+                <svg id="logo_image" viewBox="0 0 100 100">
+                    <g id="logo_path"><path d="M20 26.7l5.4-3V3.2L20 0v15.3h-6.9v-6l-5.5 3v20.5l5.5 3.2V20.7H20z"/>
+                        <path d="M28 4.6v20.8l-8 4.4V36l13-7.5v-21zM13.1 0L0 7.5v21l5.1 2.9V10.6l8-4.4z"/>
+                        </g>
+                </svg>
+                </a>
+                <Title>HashiCups</Title>
+            </Logo>
+      </Grid>
 
       <Grid item container spacing={4} justify="center">
       <Grid
