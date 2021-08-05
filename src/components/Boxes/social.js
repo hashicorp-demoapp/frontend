85fbebc7fd16d168a0c36250b6c53da6fa7cd7dc
Rajas Nathak <61021680+rajasnathak@users.noreply.github.com>
2021-08-05 15:55:18 -0500

    footer

---
+++ src/components/Boxes/social.js
@@ -0,0 +1,9 @@
+export const socialMedia = {
+    instagram: "https://www.instagram.com/hashi/",
+    facebook: "https://www.facebook.com/HashiCorp/",
+    github:
+      "https://github.com/hashicorp",
+    homepage: "",
+    
+    twitter: "https://twitter.com",
+  };
