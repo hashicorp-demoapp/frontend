eaddc50a3fe3ce44f678306fd70195573d53a786
Rajas Nathak <61021680+rajasnathak@users.noreply.github.com>
2021-08-06 11:33:21 -0500

    footer logo

--- src/components/Boxes/index.js
+++ src/components/Boxes/index.js
@@ -1,46 +1,36 @@
 import React from 'react'
 import Box from '@material-ui/core/Box';
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
-import CardMedia from '@material-ui/core/CardMedia';
+import logo from './HashiCorp_PrimaryLogo_White_RGB.png';
 
-import styled from 'styled-components'
-
-import { socialMedia } from './social';
-
-
-
-const useStyles = makeStyles((theme) => ({
-  
-
-}));
 
 
 
 
 const MuiBox = () => {
-  const classes = useStyles();
-  // if you want to add more social medias, add it to here and /data/socialMedia.js.
-  // and import the Material Icon, then add the code.
-  const {  homepage } = socialMedia;
-
-  return (<div>
+  return (
+  <div>
      
-        
+        <Box
+          border={25}
+          borderColor="black"
+          height={200}
+          width='100%'
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          bgcolor="#000000"
+          color="black"
+          fontSize={24}
+          padding = '50px'
+        >
                   
-        <Grid
-        
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <CardMedia
-        className={classes.media}
-        image="../../public/img/HashiCorp_PrimaryLogo_White_RGB.png"
-        
-/>
-      </Grid>
+                  <img src={logo} height = "40%"   />
+
+
+
+
+                  
+     </Box>
         </div>
   );
 };
