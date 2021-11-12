9ff20275ce283dd8c61de9928778b85f0f75d724
Tu Nguyen <im2nguyen@gmail.com>
2021-11-12 10:40:39 -0800

    Add vercel GH action

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -3,14 +3,14 @@ import {
     BrowserRouter as Router,
     Switch,
     Route
-  } from "react-router-dom";
+} from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
 
 import { Header } from '../../components/Header'
 import { Slider } from '../../components/Slider'
-import {Container} from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import Payment from '../Payment'
 
 const QUERY_coffees = gql`
@@ -35,6 +35,8 @@ const Coffees = styled(Slider)`
 const App = () => {
     const { loading, error, data } = useQuery(QUERY_coffees)
 
+    console.log(error)
+
     if (error) return <p>Error :(</p>
 
     return (
@@ -42,15 +44,15 @@ const App = () => {
             <Switch>
                 <Route path="/payments">
                     <Header loading={loading} />
-                    <Container style={{paddingTop: "5em"}}>
-                        <Payment/>
+                    <Container style={{ paddingTop: "5em" }}>
+                        <Payment />
                     </Container>
                 </Route>
                 <Route path="/">
                     <Header loading={loading} />
-                        {!loading && 
-                            <Coffees items={ data.coffees} />
-                        }
+                    {!loading &&
+                        <Coffees items={data.coffees} />
+                    }
                 </Route>
             </Switch>
         </Router>
